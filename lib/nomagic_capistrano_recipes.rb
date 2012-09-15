828e5643c00a318012bc1555d4649a8d4538e14d
Alexander Belozerov <belozerov.sasha@gmail.com>
2012-09-15 23:04:27 +0700

    Add tasks to stop/start/restart unicorn

--- lib/nomagic_capistrano_recipes.rb
+++ lib/nomagic_capistrano_recipes.rb
@@ -145,5 +145,20 @@ module NomagicCapistranoRecipes
         end
       end
     end
+
+    namespace :unicorn do
+      set(:unicorn_conf) { "#{deploy_to}/#{shared_dir}/config/unicorn.rb" }
+      set(:unicorn_pid) { "#{deploy_to}/#{shared_dir}/pids/unicorn.pid" }
+
+      task :restart do
+        run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+      end
+      task :start do
+        run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+      end
+      task :stop do
+        run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+      end
+    end
   end
 end
